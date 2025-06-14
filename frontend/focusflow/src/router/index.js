bdc455af5109fdc552ef43a4cad275a647166eeb
Nicolai Glock <niglit01@hs-esslingen.de>
2025-06-14 14:08:23 +0200

    add home route and redirect

--- frontend/focusflow/src/router/index.js
+++ frontend/focusflow/src/router/index.js
@@ -4,8 +4,9 @@ import HomeView from '../views/HomeView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {path:"/",redirect:"/home"},
     {
-      path: '/',
+      path: '/home',
       name: 'home',
       component: HomeView,
     },
